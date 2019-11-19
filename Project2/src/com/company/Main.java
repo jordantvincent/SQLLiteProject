5f004c74ee2251076b358e4a87f9ea228525953f
jordantvincent <46661671+jordantvincent@users.noreply.github.com>
2019-11-19 18:19:09 -0500

    Connection to database successful

--- Project2/src/com/company/Main.java
+++ Project2/src/com/company/Main.java
@@ -6,11 +6,45 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+
 public class Main {
 
     private static WebDriver driver;
 
+
+    public static void createDatabase(String filename) {
+        String url = "jdbc:sqlite:C:\\Users\\theli\\IdeaProjects\\Project2\\Database" + filename;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("Driver name: " + meta.getDriverName());
+                System.out.println("Database Created.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
+
+        //createDatabase("sqlite-jdbc-3.27.2.1.jar");
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:BannerTeachingData.db");
+            System.out.println("Successfully connected to BannerTeachingData.db!");
+
+        } catch (Exception e) {
+            //System.out.println(e.getMessage());
+            //System.exit(0);
+            System.out.println("There was a problem with connection to the database ... idiot ");
+
+        }
 
         //set up Chrome Driver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\theli\\Desktop\\chromedriver.exe");
@@ -27,12 +61,14 @@ public class Main {
         submitButton.click();
 
 
-
         Select classType = new Select(driver.findElement(By.xpath("//*[@id=\"subj_id\"]")));
         classType.selectByValue("ITEC");
 
         WebElement submitButton2 = driver.findElement(By.xpath("/html/body/div[3]/form/input[12]"));
         submitButton2.click();
+
+
     }
 }
+
 
