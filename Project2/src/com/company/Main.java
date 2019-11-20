1494b1d4ca42c55b58e0317d5cce3331e4142795
jordantvincent <46661671+jordantvincent@users.noreply.github.com>
2019-11-19 19:36:01 -0500

    Update Main.java

--- Project2/src/com/company/Main.java
+++ Project2/src/com/company/Main.java
@@ -1,27 +1,26 @@
 package com.company;
 
+import com.sun.jdi.NativeMethodException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Main {
 
     private static WebDriver driver;
+    private static String connectionUrl = "jdbc:sqlite:BannerTeachingData.db";
 
-    public static void main(String[] args) {
 
-        //createDatabase("sqlite-jdbc-3.27.2.1.jar");
+    //method that connects us to the database
+    public static void openDatabase() {
         Connection c = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:BannerTeachingData.db");
+            c = DriverManager.getConnection(connectionUrl);
             System.out.println("Successfully connected to BannerTeachingData.db!");
 
         } catch (Exception e) {
@@ -30,12 +29,115 @@ public class Main {
             System.out.println("There was a problem with connection to the database ... idiot ");
 
         }
+    }
+
+    public static void createProfessorTable() {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(connectionUrl);
+            System.out.println("Opened Database Successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE PROFESSOR " +
+                    "(ID INT PRIMARY KEY    NOT NULL," +
+                    "FIRSTNAME           TEXT    NOT NULL," +
+                    "LASTNAME            TEXT    NOT NULL," +
+                    "MAJOR               TEXT    NOT NULL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Professor Table successfully created!");
+
+    }
+
+    public static void populateProfessorTable() {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(connectionUrl);
+            c.setAutoCommit(false);
+            System.out.println("Opened Database Successfully");
+
+            stmt = c.createStatement();
+            String sql = "INSERT INTO PROFESSOR (ID,FIRSTNAME,LASTNAME, MAJOR) " +
+                    "VALUES (1, 'Tacksoo', 'Im' , 'ITEC');";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO PROFESSOR (ID,FIRSTNAME,LASTNAME, MAJOR) " +
+                    "VALUES (2, 'Cenguiz', 'Gunay' , 'ITEC');";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO PROFESSOR (ID,FIRSTNAME,LASTNAME, MAJOR) " +
+                    "VALUES (3, 'Richard', 'Price' , 'ITEC');";
+            stmt.executeUpdate(sql);
+
+
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Table successfully Populated!");
+
+    }
+
+    public static void createCourseTable() {
+        Statement stmt = null;
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(connectionUrl);
+            System.out.println("Opened Database Successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COURSE " +
+                    "(ID INT PRIMARY KEY    NOT NULL, " +
+                    "SUBJECT          TEXT    NOT NULL, " +
+                    "TITLE            TEXT    NOT NULL, " +
+                    "COURSE_NUM       TEXT    NOT NULL, " +
+                    "SEMESTER_OFFERED TEXT    NOT NULL, " +
+                    "CREDIT_HOURS     TEXT    NOT NULL, " +
+                    "NUM_STUDENTS          TEXT    NOT NULL, " +
+                    "PROFESSOR            TEXT    NOT NULL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Course Table successfully created!");
+
+}
+
+    public static void main(String[] args) {
+
+        //Opens Database Connection
+        openDatabase();
+        //Create Professor Table
+        createProfessorTable();
+        //Populate Professor Table
+        populateProfessorTable();
+        //Create Course Table
+        createCourseTable();
 
         //set up Chrome Driver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\theli\\Desktop\\chromedriver.exe");
         driver = new ChromeDriver();
 
-        //load the initial page
+        //load the initial page on Banner
         driver.get("https://ggc.gabest.usg.edu/pls/B400/bwckschd.p_disp_dyn_sched\n");
 
 
@@ -55,5 +157,6 @@ public class Main {
 
     }
 }
+
 
 
